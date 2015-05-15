0b190eaf29cb290d5bd5c2301cb501bfe7451a93
Jonas Kleinkauf <jzone123@gmail.com>
2015-05-15 23:58:09 +0200

    added SimpleCov to test helpers

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
+  gem 'quiet_assets'
+  gem 'simplecov'
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -7,4 +11,28 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+SimpleCov.at_exit do
+  SimpleCov.result.format!
+  result = (SimpleCov.result.covered_percent * $test_result)
+  File.open(Rails.root + "log/klausur.log", 'a'){|f| f.write "['#{Time.now}',#{SimpleCov.result.covered_percent},#{$test_result},#{result}]\n" }
+  puts "\n" + "=" * 34
+  puts " Projektergebnis: %.2f%% von 100%" % [result]
+  puts "=" * 34
+end
+
+module MiniTest
+  class MyReporter < MiniTest::StatisticsReporter
+    def report # :nodoc:
+      super
+      failed = failures + errors + skips
+      $test_result = (count-failed) / count.to_f
+    end
+  end
+
+  self.extensions << 'my_reporter'
+  def self.plugin_my_reporter_init opts
+    self.reporter << MiniTest::MyReporter.new
+  end
 end
