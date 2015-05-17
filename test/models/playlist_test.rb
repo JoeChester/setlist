1e569ad8a54964cd82a13acaa3d79b6d8c5d77c3
Jonas Kleinkauf <jzone123@gmail.com>
2015-05-17 23:09:43 +0200

    styled form error messages, only model tests left now

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PlaylistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'has correct associations' do
+    assert_equal :has_and_belongs_to_many, Playlist.reflect_on_association(:songs).macro
+  end
 end
